f15087af641922107ba15526e2be73cfe0b3cf98
Felipe Chernicharo <string7dev@gmail.com>
2022-05-17 17:27:27 -0400

    disable TextInput & improve data model

--- App.js
+++ App.js
@@ -7,22 +7,37 @@ import {
 	Text,
 	View,
 } from 'react-native';
+import { RectButton, TextInput } from 'react-native-gesture-handler';
 import { DATA } from './Data';
 
-const Item = ({ title }) => (
-	<View style={styles.item}>
-		<Text style={styles.title}>{title}</Text>
-	</View>
+const Item = ({ item, onClick }) => (
+	<RectButton onPress={() => onClick(item)}>
+		<View style={styles.item}>
+			<Text>{item.symbol}</Text>
+		</View>
+	</RectButton>
 );
 
 const App = () => {
+	function handleKeyPress(e) {
+		console.log(e);
+	}
 	return (
 		<SafeAreaView style={styles.container}>
+			<StatusBar barStyle="dark-content" />
+			<TextInput
+				style={styles.visor}
+				editable={false}
+				selectTextOnFocus={false}
+			></TextInput>
+
 			<FlatList
 				data={DATA}
 				numColumns={4}
 				keyExtractor={item => item.id}
-				renderItem={({ item }) => <Item title={item.title} />}
+				renderItem={({ item }) => (
+					<Item item={item} onClick={handleKeyPress} />
+				)}
 			/>
 		</SafeAreaView>
 	);
@@ -34,8 +49,8 @@ const styles = StyleSheet.create({
 		marginTop: StatusBar.currentHeight || 0,
 		justifyContent: 'center',
 		alignItems: 'center',
-		borderColor: 'red',
-		borderWidth: 2,
+		// borderColor: 'red',
+		// borderWidth: 2,
 	},
 	item: {
 		backgroundColor: '#f9c2ff',
@@ -45,8 +60,13 @@ const styles = StyleSheet.create({
 		justifyContent: 'center',
 		alignItems: 'center',
 	},
-	title: {
-		// fontSize: 32,
+	visor: {
+		height: 60,
+		width: '90%',
+		backgroundColor: '#efefef',
+		marginBottom: 16,
+		fontSize: 32,
+		textAlign: 'right',
 	},
 });
 
