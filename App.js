667f13ccd65415aa08f8478fc8cefd1448285dab
Felipe Chernicharo <string7dev@gmail.com>
2022-05-16 22:24:13 -0400

    calcultor start

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {
+	FlatList,
+	SafeAreaView,
+	StatusBar,
+	StyleSheet,
+	Text,
+	View,
+} from 'react-native';
+import { DATA } from './Data';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const Item = ({ title }) => (
+	<View style={styles.item}>
+		<Text style={styles.title}>{title}</Text>
+	</View>
+);
+
+const App = () => {
+	return (
+		<SafeAreaView style={styles.container}>
+			<FlatList
+				data={DATA}
+				numColumns={4}
+				keyExtractor={item => item.id}
+				renderItem={({ item }) => <Item title={item.title} />}
+			/>
+		</SafeAreaView>
+	);
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		marginTop: StatusBar.currentHeight || 0,
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderColor: 'red',
+		borderWidth: 2,
+	},
+	item: {
+		backgroundColor: '#f9c2ff',
+		width: 80,
+		height: 30,
+		margin: 2,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	title: {
+		// fontSize: 32,
+	},
 });
+
+export default App;
---
+++ Data.js
@@ -0,0 +1,82 @@
+export const DATA = [
+	{
+		id: 'clear',
+		title: 'C',
+	},
+	{
+		id: 'prec',
+		title: '%',
+	},
+	{
+		id: 'sqrt',
+		title: '√',
+	},
+	{
+		id: 'back',
+		title: '⌫',
+	},
+	{
+		id: 1,
+		title: '1',
+	},
+	{
+		id: 2,
+		title: '2',
+	},
+	{
+		id: 3,
+		title: '3',
+	},
+	{
+		id: 'div',
+		title: '÷',
+	},
+	{
+		id: 4,
+		title: '4',
+	},
+	{
+		id: 5,
+		title: '5',
+	},
+	{
+		id: 6,
+		title: '6',
+	},
+	{
+		id: 'mult',
+		title: 'X',
+	},
+	{
+		id: 7,
+		title: '7',
+	},
+	{
+		id: 8,
+		title: '8',
+	},
+	{
+		id: 9,
+		title: '9',
+	},
+	{
+		id: 'sub',
+		title: '-',
+	},
+	{
+		id: 'null',
+		title: '',
+	},
+	{
+		id: 0,
+		title: '0',
+	},
+	{
+		id: 'sum',
+		title: '+',
+	},
+	{
+		id: 'equals',
+		title: '=',
+	},
+];
